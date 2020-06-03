71f1e6f6c2bf713883897f0de3748c53e6e48f19
Lord-Fifth <aditya.suresh098@gmail.com>
2020-06-03 20:42:26 +0530

    Homepage Setup

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 let express = require("express")
+let app = express()
+let path = require("path");
+app.use(express.urlencoded({ extended: false }))
+
+// viewed at http://localhost:3000
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.post("/create-item", function(req, res) {
+    console.log(req.body.item)
+    res.send("Thanks for submitting this form")
+})
+
+app.listen(3000)
