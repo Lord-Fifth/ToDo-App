1a5506e19621f76f64503443f8388210feb1d80d
Lord-Fifth <aditya.suresh098@gmail.com>
2020-06-06 19:52:27 +0530

    MongoDB Create Axios

--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,34 @@
+function itemTemplate(item) {
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+        <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+        <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+    </li>
+    `
+}
+
+//Create feature
+
+let createField = document.getElementById("create-field")
+document.getElementById("create-form").addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    //Send new data to Node server using axios promise
+    axios.post("/create-item", { text: createField.value }).then(function(response) {
+
+        //Update User Interface with new data
+        document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
+
+        //Better User Experience
+        createField.value = ""
+        createField.focus()
+    }).catch(function() {
+        console.log("Please try again later.")
+    })
+})
+
 //ToDo list buttons
 document.addEventListener("click", function(e) {
     //Update feature
